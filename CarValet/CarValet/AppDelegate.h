082ea03140bf28aef3d1117e477cbd7db3d08a39
Andrew Clawson <andrewjclawson18@gmail.com>
2016-04-02 23:00:39 -0600

    Initial commit.

---
+++ CarValet/CarValet/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CarValet
+//
+//  Created by Andrew Clawson on 4/2/16.
+//  Copyright © 2016 Andrew Clawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CarValet/CarValet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarValet
+//
+//  Created by Andrew Clawson on 4/2/16.
+//  Copyright © 2016 Andrew Clawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
